1de35a32997c6a31606c6c2cc57ac4872dce4a1c
MenaceG <ayushg72963@gmail.com>
2024-04-02 13:54:12 +0530

    Tesla Clone Deployement

--- tesla_clone/script.js
+++ tesla_clone/script.js
@@ -3,17 +3,19 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slide");
+    let i;
+    let slides = document.getElementsByClassName("slide");
 
-  for (i = 0; i < slides.length; i++){
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
 
-  slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
 
-  setTimeout(showSlides, 2000);
+    setTimeout(showSlides, 6000);
 
 }
